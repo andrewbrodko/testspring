1b7850c67bba383f4fb7f1a1a44a98f8aca2d807
Andrew Brodko <work.andrewbrodko@gmail.com>
2019-12-16 19:07:09 +0300

    FileUpload xls parse testing

--- src/main/java/com/example/testspring/controller/FileUploadController.java
+++ src/main/java/com/example/testspring/controller/FileUploadController.java
@@ -1,6 +1,7 @@
 package com.example.testspring.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.NoSuchElementException;
@@ -9,6 +10,10 @@ import java.util.HashMap;
 
 import com.example.testspring.model.FileUpload;
 import com.example.testspring.repository.FileUploadRepository;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Async;
@@ -28,7 +33,7 @@ public class FileUploadController
             method = RequestMethod.POST,
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 
-    public Map<String, String> fileUpload(@RequestParam("file") MultipartFile file) throws IOException{
+    public Map<String, Long> fileUpload(@RequestParam("file") MultipartFile file) throws IOException{
         String pathname = "C:/Users/SD_CODEPC/Documents/upload/" + file.getOriginalFilename();
         File convertFile = new File(pathname);
 
@@ -45,12 +50,12 @@ public class FileUploadController
             fout.close();
             fileUpload.setStatus("DONE");
         } catch (IOException exe) {
-            exe.printStackTrace();
             fileUpload.setStatus("ERROR");
         }
+        readFromExcel(pathname);
 
-        Map<String, String> resp = new HashMap<>();
-        resp.put("status", fileUploadRepository.save(fileUpload).getStatus());
+        Map<String, Long> resp = new HashMap<>();
+        resp.put("id", fileUploadRepository.save(fileUpload).getId());
 
         return resp;
     }
@@ -65,6 +70,20 @@ public class FileUploadController
             resp.put("status", "NOT EXISTS");
         }
         return resp;
+    }
+
+    public static void readFromExcel(String file) throws IOException{
+        HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream(file));
+        HSSFSheet myExcelSheet = myExcelBook.getSheet("Sheet1");
+        HSSFRow row = myExcelSheet.getRow(0);
+
+        if(row.getCell(0).getCellType() == HSSFCell.CELL_TYPE_STRING){
+            String name = row.getCell(0).getStringCellValue();
+            System.out.println("name : " + name);
+        }
+
+        myExcelBook = null;
+        return;
     }
 
 //    @GetMapping("/import/{fileId}")
